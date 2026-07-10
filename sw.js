52fdba3a573e9e1e757509686333539f1406e7e6
COGERH-AP <diegomaronese@gmail.com>
2026-07-10 12:23:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const ASSETS = [
   './icon.svg',
   './cogerh_logo.png',
   './utfpr_logo.svg',
+  './utfpr_logo_b.svg',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
